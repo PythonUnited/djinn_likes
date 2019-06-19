ebee7c560fba0c63f59ccba654d123d159d28570
Martin Bouma <martin@studioreactor.com>
2019-06-19 11:02:52 +0200

    raw_id_fields in admin

--- djinn_likes/admin.py
+++ djinn_likes/admin.py
@@ -6,6 +6,7 @@ class LikeAdmin(admin.ModelAdmin):
 
     list_display = ('content_object', 'user', 'created', )
     list_filter = ['created', ]
+    raw_id_fields = ['user']
     search_fields = ['user__username', ]
 
     readonly_fields = ['created']
